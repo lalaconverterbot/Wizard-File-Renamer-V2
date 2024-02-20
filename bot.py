891cbb9aad1ed4aa290c6dc288a6f006e0a82453
lalaconverterbot <145864064+lalaconverterbot@users.noreply.github.com>
2024-02-19 23:21:46 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,11 +4,11 @@ import os
 
 from plugins.cb_data import app as Client2
 
-TOKEN = os.environ.get("TOKEN", "")
+TOKEN = os.environ.get("TOKEN", "6569891735:AAGmrfy-CF3_scVWMPV8OSwwvWImbCAPihM")
 
-API_ID = int(os.environ.get("API_ID", ""))
+API_ID = int(os.environ.get("API_ID", "14084834"))
 
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "3370f5b5ed3ac5ad6931724413f529f6")
 
 STRING = os.environ.get("STRING", "")
 
